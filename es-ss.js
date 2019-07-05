c867a9e9a5c7809575b13e2f2abf0bb107551c44
Timothy McCallum <tpmccallum@users.noreply.github.com>
2019-07-06 09:43:46 +1000

    Create callable Javascript class
    This has the following functions:
    
    `submitAbi(_abi, _transactionHash)`
    
    `shaAbi(_abi)`
    
    `searchUsingAbi(_abiHash)`
    
    `searchUsingKeywords(_keywords)`
    
    `searchUsingKeywordsAndAbi(_abiHash, keywords)`

---
+++ es-ss.js
@@ -0,0 +1,51 @@
+class ESSS {
+	// Search Engine Base URL (Please include protocol. Please do not include trailing slash)
+	// Example: https://search-engine.com
+    constructor(_searchEngineBaseUrl) {
+        this.searchEngineBaseUrl = _searchEngineBaseUrl;
+        console.log("Search Engine Base URL set to: " + this.searchEngineBaseUrl);
+    } 
+
+    submitAbi(_abi, _transactionHash){
+    	// request initialisation
+    	var xhr = new XMLHttpRequest();
+    	var url = this.searchEngineBaseUrl + "/api/submit_abi";
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		//data
+		var data = {};
+		data["abi"] = _abi;
+		data["hash"] = _transactionHash;
+		//execution
+		xhr.onload = function (e) {
+		  if (xhr.readyState === 4) {
+		    if (xhr.status === 200) {
+		      console.log(xhr.responseText);
+		    } else {
+		      console.error(xhr.statusText);
+		    }
+		  }
+		};
+		xhr.onerror = function (e) {
+		  console.error(xhr.statusText);
+		};
+		xhr.send(JSON.stringify(data));
+    }
+
+    shaAbi(){
+
+    }
+
+    searchUsingAbi(){
+
+    }
+
+    searchUsingKeywords(){
+
+    }
+
+    searchUsingKeywordsAndAbi(){
+
+    }
+
+}
