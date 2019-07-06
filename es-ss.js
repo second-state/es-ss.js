e57eec1476f504e06fa4becb1865918a5cd393f6
Timothy McCallum <tpmccallum@users.noreply.github.com>
2019-07-06 10:11:16 +1000

    Format syntax

--- es-ss.js
+++ es-ss.js
@@ -1,50 +1,50 @@
 class ESSS {
-	// Search Engine Base URL (Please include protocol. Please do not include trailing slash)
-	// Example: https://search-engine.com
+    // Search Engine Base URL (Please include protocol. Please do not include trailing slash)
+    // Example: https://search-engine.com
     constructor(_searchEngineBaseUrl) {
         this.searchEngineBaseUrl = _searchEngineBaseUrl;
         console.log("Search Engine Base URL set to: " + this.searchEngineBaseUrl);
-    } 
-
-    submitAbi(_abi, _transactionHash){
-    	// request initialisation
-    	var xhr = new XMLHttpRequest();
-    	var url = this.searchEngineBaseUrl + "/api/submit_abi";
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-Type", "application/json");
-		//data
-		var data = {};
-		data["abi"] = _abi;
-		data["hash"] = _transactionHash;
-		//execution
-		xhr.onload = function (e) {
-		  if (xhr.readyState === 4) {
-		    if (xhr.status === 200) {
-		      console.log(xhr.responseText);
-		    } else {
-		      console.error(xhr.statusText);
-		    }
-		  }
-		};
-		xhr.onerror = function (e) {
-		  console.error(xhr.statusText);
-		};
-		xhr.send(JSON.stringify(data));
     }
 
-    shaAbi(){
-
+    submitAbi(_abi, _transactionHash) {
+        // request initialisation
+        var xhr = new XMLHttpRequest();
+        var url = this.searchEngineBaseUrl + "/api/submit_abi";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        //data
+        var data = {};
+        data["abi"] = _abi;
+        data["hash"] = _transactionHash;
+        //execution
+        xhr.onload = function(e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log(xhr.responseText);
+                } else {
+                    console.error(xhr.statusText);
+                }
+            }
+        };
+        xhr.onerror = function(e) {
+            console.error(xhr.statusText);
+        };
+        xhr.send(JSON.stringify(data));
     }
 
-    searchUsingAbi(){
+    shaAbi(_abi) {
 
     }
 
-    searchUsingKeywords(){
+    searchUsingAbi(_abiHash) {
 
     }
 
-    searchUsingKeywordsAndAbi(){
+    searchUsingKeywords(_keywords) {
+
+    }
+
+    searchUsingKeywordsAndAbi(_abiHash, keywords) {
 
     }
 
