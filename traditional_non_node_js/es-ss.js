6fc3f98eef4aa2a423872ecb6b1c74db1fc07f53
tpmccallum <mistermac2008@gmail.com>
2019-09-03 14:52:17 +1000

    Traditionsl JS, adding new general query function; es-ss.js can take native ES DSL query as input

--- traditional_non_node_js/es-ss.js
+++ traditional_non_node_js/es-ss.js
@@ -19,6 +19,29 @@ class ESSS {
         return this.indexStatus[_transactionHash];
     }
 
+    queryUsingDsl(_query) {
+        var url = this.searchEngineBaseUrl + "/api/es_search";
+        return new Promise(function(resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            //data
+            var data = {};
+            data["myQuery"] = _abi;
+            xhr.onload = function(e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        resolve(xhr.responseText);
+                    }
+                }
+            };
+            xhr.onerror = reject;
+            xhr.open("POST", url, true);
+            xhr.send(JSON.stringify(data));
+        });
+    }
+
     updateStateOfContractAddress(_abi, _address) {
         var url = this.searchEngineBaseUrl + "/api/update_state_of_contract_address";
         return new Promise(function(resolve, reject) {
